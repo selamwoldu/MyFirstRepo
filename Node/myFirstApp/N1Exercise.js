88b2e443c90d5adec830a920d80ca365dbddf8cf
Selamawit <selamawino4@gmail.com>
2021-06-04 10:32:31 -0500

    Node Exercises

---
+++ Node/myFirstApp/N1Exercise.js
@@ -0,0 +1,8 @@
+//N1 Exercise
+
+const http = require('http');
+http.createServer(function (req, res) {
+res.writeHead(200,
+      {'Content-Type': 'text/html'});
+res.end('Hello World!');
+}).listen(8090);
---
+++ Node/myFirstApp/N2Exercise.js
@@ -0,0 +1,10 @@
+// //N2 Exercise
+
+var http = require('http');
+var dt = require('./myModule');
+http.createServer(function (req, res) {
+  res.writeHead(200,
+          {'Content-Type': 'text/html'});
+  res.write("The date and time are currently: " + dt.myDate());
+  res.end();
+}).listen(8090);
